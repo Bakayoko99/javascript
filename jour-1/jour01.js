b0cb4c1a92c72bbe7cb279aa7a30fc6304bbee2f
unknown <you@example.com>
2021-03-08 16:02:19 +0100

    jour01 js

---
+++ jour-1/jour01.js
@@ -0,0 +1,54 @@
+// 01 - Hello Word
+
+console.log("Hello World !");
+
+// 02 - String
+
+var test = "My name is Bakagnan";
+
+console.log(test);
+
+// 03 - Concatenation
+
+var name = "Bakagnan";
+
+console.log("Nice to meet you " + name);
+
+// 04 - String Length
+
+var testLength = "I'm very long !";
+
+console.log(testLength.length);
+
+// 05 - Replace
+
+var food = "croissant is meh";
+
+console.log(food.replace("meh", "so good"));
+
+// 06 - Up and Down
+
+var basic = "This is cool";
+
+var basicUp = basic.toLocaleUpperCase();
+
+var basicDown = basic.toLocaleLowerCase();
+
+console.log(basic, basicUp, basicDown);
+
+// 07 - Split
+
+var word = "banana";
+
+var letters = word.split([""]);
+
+console.log(letters);
+
+// 08 - Template
+
+var age = 22;
+
+var template = `i'm ${age} years old`;
+
+console.log(template);
+
