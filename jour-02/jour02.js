f7bc0db019992316004180b0ee36cd2f27d5d8df
unknown <you@example.com>
2021-03-09 17:29:44 +0100

    jour02 bonus

--- jour-02/jour02.js
+++ jour-02/jour02.js
@@ -66,39 +66,142 @@ var limit = 50;
 
 var score = 64;
 
-if(score >= limit){
+if (score >= limit) {
 
     console.log("Ok good !")
 
-}else(console.log("Oh nooo..."));
+} else (console.log("Oh nooo..."));
 
 // 07 - Condition II
 
-var password = "azert";
+var password = "azerty";
 
-if( password.length > 5 ){
+if (password.length > 5) {
 
     console.log("The password is secure")
 
-}else(console.log("The password is not secure"));
+} else (console.log("The password is not secure"));
 
 // 08 - Condition III
 
-if( score >= limit && password.length > 5 ){
+if (score >= limit && password.length > 5) {
 
     console.log("Everything is good")
 
-} 
-else if( score >= limit || password.length > 5 ){
+}
+else if (score >= limit || password.length > 5) {
 
     console.log("Something is good")
 
-} 
-else if( score >= limit === password.length > 5 ){
+}
+else if (score >= limit === password.length > 5) {
 
     console.log("Nothing is good")
 
 }
+
+// 09 - Bonus
+
+var random = 10;
+
+if (random === 6) {
+
+    console.log("Yes I win !")
+
+}
+
+else if (random != 6) {
+
+    console.log("So close...")
+}
+
+
+switch (random) {
+    case 6:
+        console.log("Yes I win !");
+        break;
+    case 1:
+        console.log("So close...");
+        break;
+    case 2:
+        console.log("So close...");
+        break;
+    case 3:
+        console.log("So close...");
+        break;
+    case 4:
+        console.log("So close...");
+        break;
+    case 5:
+        console.log("So close...");
+        break;
+    default:
+        console.log("not a valid number");
+}
+
+// 10 - Bonus II
+
+var month = "january"
+
+switch (month) {
+    case "january":
+        console.log("Winter");
+        break;
+    case "februay":
+        console.log("Winter");
+        break;
+    case "mars":
+        console.log("Spring");
+        break;
+    case "april":
+        console.log("Spring");
+        break;
+    case "may":
+        console.log("Summer");
+        break;
+    case "june":
+        console.log("Summer");
+        break;
+    case "july":
+        console.log("Summer");
+        break;
+    case "august":
+        console.log("Summer");
+        break;
+    case "september":
+        console.log("Fall");
+        break;
+    case "october":
+        console.log("Fall");
+        break;
+    case "november":
+        console.log("Winter");
+        break;
+    case "december":
+        console.log("Winter");
+        break;
+    default:
+        console.log("That's not a month...");
+}
+
+// 11 - Bonus III
+
+var roundeNumber = 3.6
+
+if( roundeNumber   ){
+
+    console.log(Math.floor())
+
+}
+
+else if( ){
+    console.log(Math.ceil())
+}
+
+
+
+// 12 - Bonus IV
+
 
 
 
