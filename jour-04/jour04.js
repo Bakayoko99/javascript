d6f3712a4bd1cba06b022568a9eb0c023c20a16c
unknown <you@example.com>
2021-03-11 17:31:06 +0100

    jour04 time

--- jour-04/jour04.js
+++ jour-04/jour04.js
@@ -40,13 +40,60 @@ console.log(cat2.isCute);
 
 function checkIfEven(num){
 
-    
+    if( num % 2 === 0){
+        console.log("even")
+    }
 
-}
+    else(console.log("odd"))
+
+};
+
+checkIfEven(6);
+
+checkIfEven(8);
+
+checkIfEven(13);
 
 // 04 - Compare
 
+function compare(num1, num2){
+
+    if(num1 > num2){
+        console.log("num1 is bigger")
+    }
+
+    else if(num2 > num1){
+        console.log("num2 is bigger")
+    }
+
+    else(console.log("both are the same"))
+};
+
+compare(44, 44);
+
+compare(12, 44);
+
+compare(36, 25);
+
 // 05 - Add Up
+
+
+
+function addUp(num){
+
+    var a = 0;
+
+    for (var i = 1; i <= num; i++ ) {
+
+       a += i;
+    
+    }
+
+    return a;
+
+};
+
+console.log(addUp(12));
 
 // 06 - Time
 
