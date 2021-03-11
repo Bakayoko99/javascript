6a8e43cff62239d46517e5ae4e06655f354bae9e
unknown <you@example.com>
2021-03-11 16:01:12 +0100

    jour04

---
+++ jour-04/jour04.js
@@ -0,0 +1,56 @@
+
+// 01 - Object
+
+var cat = {
+
+    name: "Garfield",
+
+    age: 3,
+
+    isCute: true
+};
+
+console.log(cat);
+
+console.log(cat.age);
+
+if( cat.isCute === true){
+    console.log("So cute !")
+};
+
+// 02 - Combine
+
+var cat2 = {
+    
+    name: "ronaldo",
+
+    age: 32,
+
+    isCute: false
+
+}
+
+var cats = [ cat, cat2 ];
+
+console.log(cat.age);
+
+console.log(cat2.isCute);
+
+// 03 - Even
+
+function checkIfEven(num){
+
+    
+
+}
+
+// 04 - Compare
+
+// 05 - Add Up
+
+// 06 - Time
+
+// 07 - Bonus 
+
+// 08 - Bonus II
+
