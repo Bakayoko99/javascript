e3e10e35327f42f76f8252ca56f1dbd172f437c8
unknown <you@example.com>
2021-03-15 17:27:33 +0100

    revisions exo1-2

---
+++ jour-05/revisions/exo1.js
@@ -0,0 +1,24 @@
+
+
+var string = "";
+
+function sortLetters(string){
+
+    var tableau =  string.split("")
+
+    tableau.sort()
+
+    console.log(tableau)
+
+
+}
+
+sortLetters("konexio")
+
+
+
+
+
+
+
+
